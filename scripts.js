3e1c318d302f47bc66f6b2d887a37a3066611f28
mbharrat <mbharrat@gmail.com>
2018-10-20 19:42:24 -0400

    FIXED DRAG AND DROP

--- scripts.js
+++ scripts.js
@@ -4,36 +4,50 @@
 
     //alert('ready!');
     //template for new table row
-    const templateRow = '<tr class="event"draggable="true"><td></td><td></td></tr>';
-    $.ajax({url: "https://jsonplaceholder.typicode.com/albums?userId=1", success: function(result){
-        //dynamic adding of special div rows
-        for(album in result){
+const templateRow = '<tr class="event"draggable="true"><td></td><td></td></tr>';
+    //convert to a get
+let table1 = (number) =>{
+
+	$.get("https://jsonplaceholder.typicode.com/albums?userId="+number, function(response){
+    	
+    }).done(function(response){
+    	for(album in response){
         	 $(".connectedSortable").append(templateRow);
-        	 var tRow = $('.connectedSortable > tr:eq('+(parseInt(album, 10)+1)+')').attr("id","U1:"+album);
+        	 var tRow = $('.connectedSortable > tr:eq('+(parseInt(album, 10)+1)+')').attr("id","U1"+album);
         	 var temp = $('.connectedSortable > tr:eq('+(parseInt(album, 10)+1)+') > td').attr("id","idUser1"+album);
         	 var albumTemp = $('.connectedSortable > tr:eq('+(parseInt(album, 10)+1)+') > td:eq(1)').attr("id","albumUser1"+album);
-        	 $("#idUser1"+album).html(result[album].id);
-        	 $("#albumUser1"+album).html(result[album].title);
+        	 $("#idUser1"+album).html(response[album].id);
+        	 $("#albumUser1"+album).html(response[album].title);
      
         }
+    })
+};
 
-    }});
-     
-
-     $.ajax({url: "https://jsonplaceholder.typicode.com/albums?userId=2", success: function(result){
-        //dynamic adding of special div rows
-        for(album in result){
+let table2 = (number) => {
+	$.get("https://jsonplaceholder.typicode.com/albums?userId=2", function(response){
+    	
+    }).done(function(response){
+    	for(album in response){
         	 $(".connectedSortable2").append(templateRow);
+        	 var tRow = $('.connectedSortable2 > tr:eq('+(parseInt(album, 10)+1)+')').attr("id","U2"+album);
         	 var temp = $('.connectedSortable2 > tr:eq('+(parseInt(album, 10)+1)+') > td').attr("id","idUser2"+album);
         	 var albumTemp = $('.connectedSortable2 > tr:eq('+(parseInt(album, 10)+1)+') > td:eq(1)').attr("id","albumUser2"+album);
-        	 $("#idUser2"+album).html(result[album].id);
-        	 $("#albumUser2"+album).html(result[album].title);
+        	 $("#idUser2"+album).html(response[album].id);
+        	 $("#albumUser2"+album).html(response[album].title);
      
         }
+    });
+};
 
-    }});
 
-     $(document).ready(function(){
+
+
+table1(1);
+table2(2);
+
+$(document).ready(function(){
+
+
     //testing drag and drop
 
      $('.event').on("dragstart", function (event) {
@@ -48,21 +62,23 @@
     $('tbody tr').on("dragenter dragover drop", function (event) {
         event.preventDefault();
         
-        if (event.type === 'drop') {
+        if (event.type == 'drop') {
             var data = event.originalEvent.dataTransfer.getData('Text', $(this).attr('id'));
+            de = $('#' + data).detach(); 
             
-            var de = $('#' + data).detach();
            
     		//console.log(event.originalEvent.target);
             if (event.originalEvent.target.tagName == "TR") {
             	//console.log(event.originalEvent.target);
             	console.log("hit");
-                de.insertBefore($(event.originalEvent.target));
+                //de.insertBefore($(event.originalEvent.target));
             }
             else {
-            	console.log($(this));
-                var txt = "<p>test</p>";
-                $(this).after(txt);
+            	//console.log($(this));
+                var txt = "<tr><td></td>test<td>test</td></tr>";
+                
+                $(this).after(de);
+                
             }
         };
 
@@ -71,9 +87,6 @@
 
 	
 
-	
-
-  
 
    
 });
