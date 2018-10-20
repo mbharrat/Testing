9b1d85945bd7ae7fd8d5f23aaf1ede5058dad031
mbharrat <mbharrat@gmail.com>
2018-10-20 16:04:58 -0400

    slight breakthrough in adding to new list

--- scripts.js
+++ scripts.js
@@ -1,14 +1,15 @@
-$(document).ready(function(){
 
-	
+
+
 
     //alert('ready!');
     //template for new table row
-    const templateRow = "<tr><td></td><td></td></tr>";
+    const templateRow = '<tr class="event"draggable="true"><td></td><td></td></tr>';
     $.ajax({url: "https://jsonplaceholder.typicode.com/albums?userId=1", success: function(result){
         //dynamic adding of special div rows
         for(album in result){
         	 $(".connectedSortable").append(templateRow);
+        	 var tRow = $('.connectedSortable > tr:eq('+(parseInt(album, 10)+1)+')').attr("id","U1:"+album);
         	 var temp = $('.connectedSortable > tr:eq('+(parseInt(album, 10)+1)+') > td').attr("id","idUser1"+album);
         	 var albumTemp = $('.connectedSortable > tr:eq('+(parseInt(album, 10)+1)+') > td:eq(1)').attr("id","albumUser1"+album);
         	 $("#idUser1"+album).html(result[album].id);
@@ -31,6 +32,46 @@ $(document).ready(function(){
         }
 
     }});
+
+     $(document).ready(function(){
+    //testing drag and drop
+
+     $('.event').on("dragstart", function (event) {
+        
+        var dt = event.originalEvent.dataTransfer;
+        dt.setData('Text', $(this).attr('id'));
+        //console.log($(this).attr('id'));
+         
+    });
+
+     
+    $('tbody tr').on("dragenter dragover drop", function (event) {
+        event.preventDefault();
+        
+        if (event.type === 'drop') {
+            var data = event.originalEvent.dataTransfer.getData('Text', $(this).attr('id'));
+            
+            var de = $('#' + data).detach();
+           
+    		//console.log(event.originalEvent.target);
+            if (event.originalEvent.target.tagName == "TR") {
+            	//console.log(event.originalEvent.target);
+            	console.log("hit");
+                de.insertBefore($(event.originalEvent.target));
+            }
+            else {
+            	console.log($(this));
+                var txt = "<p>test</p>";
+                $(this).after(txt);
+            }
+        };
+
+    });
+    
+
+	
+
+	
 
   
 
