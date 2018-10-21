56e9161573f02773e020ec3cadad47b02f5f9e55
mbharrat <mbharrat@gmail.com>
2018-10-21 03:05:50 -0400

    weird behavior because clashing id

--- scripts.js
+++ scripts.js
@@ -4,9 +4,9 @@
 
     //alert('ready!');
     //template for new table row
-const templateRow = '<tr class="event"draggable="true"><td></td><td></td></tr>';
+const templateRow = '<tr class="event"draggable="true"><td></td><td></td><td style="display:none"></td></tr>';
     //convert to a get
-let table1 = (number, number2) =>{
+let table = (number, number2) =>{
 
 	$.get("https://jsonplaceholder.typicode.com/albums?userId="+number, function(response){
     	$('#mainItem').css('display','none');
@@ -15,11 +15,12 @@ let table1 = (number, number2) =>{
     }).done(function(response){
         for(album in response){
         	 $(".connectedSortable").append(templateRow);
-        	 var tRow = $('.connectedSortable > tr:eq('+(parseInt(album, 10)+1)+')').attr("id","U1"+album);
-        	 var temp = $('.connectedSortable > tr:eq('+(parseInt(album, 10)+1)+') > td').attr("id","idUser1"+album);
-        	 var albumTemp = $('.connectedSortable > tr:eq('+(parseInt(album, 10)+1)+') > td:eq(1)').attr("id","albumUser1"+album);
-        	 $("#idUser1"+album).html(response[album].id);
-        	 $("#albumUser1"+album).html(response[album].title);
+        	 var tRow = $('.connectedSortable > tr:eq('+(parseInt(album, 10)+1)+')').attr("id","U"+number+''+album);
+        	 var temp = $('.connectedSortable > tr:eq('+(parseInt(album, 10)+1)+') > td').attr("id","idUser"+number+''+album);
+        	 var albumTemp = $('.connectedSortable > tr:eq('+(parseInt(album, 10)+1)+') > td:eq(1)').attr("id","albumUser"+number+''+album);
+            var userIdTemp = $('.connectedSortable > tr:eq('+(parseInt(album, 10)+1)+') > td:eq(2)').attr("id",response[album].userId);
+        	 $("#idUser"+number+''+album).html(response[album].id);
+        	 $("#albumUser"+number+''+album).html(response[album].title);
      
         }
 
@@ -32,17 +33,60 @@ let table1 = (number, number2) =>{
                 $('.loader').css('display','none');
             for(album in response){
                  $(".connectedSortable2").append(templateRow);
-                 var tRow = $('.connectedSortable2 > tr:eq('+(parseInt(album, 10)+1)+')').attr("id","U2"+album);
-                 var temp = $('.connectedSortable2 > tr:eq('+(parseInt(album, 10)+1)+') > td').attr("id","idUser2"+album);
-                 var albumTemp = $('.connectedSortable2 > tr:eq('+(parseInt(album, 10)+1)+') > td:eq(1)').attr("id","albumUser2"+album);
-                 $("#idUser2"+album).html(response[album].id);
-                 $("#albumUser2"+album).html(response[album].title);
+                 var tRow = $('.connectedSortable2 > tr:eq('+(parseInt(album, 10)+1)+')').attr("id","U"+number2+''+album);
+                 var temp = $('.connectedSortable2 > tr:eq('+(parseInt(album, 10)+1)+') > td').attr("id","idUser"+number2+''+album);
+                 var albumTemp = $('.connectedSortable2 > tr:eq('+(parseInt(album, 10)+1)+') > td:eq(1)').attr("id","albumUser"+number2+''+album);
+                 var userIdTemp = $('.connectedSortable2 > tr:eq('+(parseInt(album, 10)+1)+') > td:eq(2)').attr("id",response[album].userId);
+                 $("#idUser"+number2+''+album).html(response[album].id);
+                 $("#albumUser"+number2+''+album).html(response[album].title);
      
             }
+            drag()
+    
+    //add css
+    
+    
+    });
+
         
+    })
+    
+
+};
+
+//post function
+
+let postAlbum = (element,uId,t) => {
+    $.post("https://jsonplaceholder.typicode.com/posts", { "userId": uId.toString(), "title": t.toString() })
+    .done(function(response){
+        console.log(response);
+        element.after(templateRow);
+        var tRow = $('#'+element[0].id +'+ tr').attr("id","U"+response.userId+''+response.id);
+        var temp = $('#'+element[0].id+' +tr > td').attr("id","idUser"+response.userId+''+response.id);
+        var albumTemp = $('#'+element[0].id+'+tr > td:eq(1)').attr("id","albumUser"+response.userId+''+response.id);
+        var userIdTemp = $('#'+element[0].id+'+tr > td:eq(2)').attr("id",response.userId);
+        
+
+        $("#idUser"+response.userId+''+response.id).html(response.id);
+        $("#albumUser"+response.userId+''+response.id).html(response.title);
+        $('#mainItem').css('display','');
+        $('.loader').css('display','none');
+
+        //reset handler?
+        drag();
+    
+        
+        
+    });
+};
+
+//set up my dragging handlers
+const drag = () => {
+
         $('.event').on("dragstart", function (event) {
-        
+            
             var dt = event.originalEvent.dataTransfer;
+
             dt.setData('Text', $(this).attr('id'));
             var animate = event.originalEvent.dataTransfer.getData('Text', $(this).attr('id'));
             animate = $('#'+animate);
@@ -61,9 +105,10 @@ let table1 = (number, number2) =>{
      
         $('tbody tr').on("dragenter dragover drop", function (event) {
             event.preventDefault();
-            
+                
             if (event.type == 'drop') {
                 var data = event.originalEvent.dataTransfer.getData('Text', $(this).attr('id'));
+
                 de = $('#' + data).detach(); 
                 
                
@@ -73,31 +118,29 @@ let table1 = (number, number2) =>{
                     console.log("hit");
                     //de.insertBefore($(event.originalEvent.target));
                 }else {
-                    //console.log($(this));
-                    //var txt = "<tr><td></td>test<td>test</td></tr>";
                     
+                    var item = $('#'+$(this)[0].id);
+                    console.log(data);
+                    $('#mainItem').css('display','none');
+                    $('.loader').css('display','');
+                    var userId = item[0].childNodes[2].id
+                    var title = de[0].childNodes[1].innerHTML
+                    postAlbum(item,userId, title);
                     var unAnimate= $(this).parent().parent()[0].id;
                     $('#'+unAnimate).removeClass('shake');
-                    $(this).after(de);
+                   // $(this).after(de);
+
                     
                 }
             };
 
         });
-    //add css
-    
-    
-    });
-
-        
-    })
-    
-
 };
 
 
 
-  table1(1,2);
+  table(3,2);
+ 
 
 
 
